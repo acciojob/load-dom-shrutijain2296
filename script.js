8779665bf1cb5d57108c3f3026dc80567051b5fb
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-18 13:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 //your JS code here. If required.
-// document.addEventListener("DOMContentLoaded", function(){
-// 	let text = document.createElement("h1");
-// 	text.innerText = "DOM load success";
-// 	document.body.appendChild(text);
-// });
-
 document.addEventListener("DOMContentLoaded", function(){
-    if (typeof Cypress !== 'undefined') {
-        let text = document.createElement("h1");
-        text.innerText = "DOM load success";
-        document.body.appendChild(text);
-    } else {
-        console.log("DOM load success");
-    }
+	let text = document.createElement("h1");
+	text.innerText = "DOM load success";
+	document.body.appendChild(text);
+});
+
+
