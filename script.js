cefee7e0ed2694c12c670fe67d002931d8fe1740
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-18 12:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function(){
 	let text = document.getElementById("dom");
 	text.innerText = "DOM load success";
-};
+});
